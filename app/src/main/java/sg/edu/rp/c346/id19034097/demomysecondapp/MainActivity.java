44d47f9e981d7fffefd45d5d49c9fe761f5bece3
LI TONG <19034097@myrp.edu.sg>
2020-05-04 12:34:16 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/id19034097/demomysecondapp/MainActivity.java
@@ -0,0 +1,14 @@
+package sg.edu.rp.c346.id19034097.demomysecondapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
